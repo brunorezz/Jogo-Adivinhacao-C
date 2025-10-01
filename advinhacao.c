64b3ec80b62be8306610ad3a9332f4dbf1065cbf
Bruno <bruno765rez@gmail.com>
2025-10-01 12:56:33 -0300

    first commit

---
+++ advinhacao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    //Declara as variavéis
+    int num = 42;
+    int advinhar; 
+    
+    // Imprime o cabeçalho do nosso jogo
+    printf("=========================================\n");
+    printf("= Bem vindo ao nosso jogo de advinhação =\n");
+    printf("=========================================\n");
+    printf("O número %d é o secreto", num);
+    return 0;
+}
