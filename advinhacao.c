690d7e6281a660ca81a61a98210cff9f93f37594
Bruno <bruno765rez@gmail.com>
2025-10-06 09:51:55 -0300

    adiciona vidas

--- advinhacao.c
+++ advinhacao.c
@@ -11,25 +11,27 @@ int main() {
     printf("= Seja bem vindo ao jogo de advinhação =\n");
     printf("========================================\n");
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
-
-    int acertou = (chute == numerosecreto);
-
-    if (acertou) {
-        printf("Parabéns! Você acertou!\n");
-    } else {
-
-        int maior = chute > numerosecreto;
-
-        if(maior) {
-            printf("Seu número é maior que o número secreto");
+    for(int i = 1; i <= 3; i++) {
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+    
+        int acertou = (chute == numerosecreto);
+    
+        if (acertou) {
+            printf("Parabéns! Você acertou!\n");
+            return 0;
         } else {
-            printf("Seu número é menor que o número secreto");
+    
+            int maior = chute > numerosecreto;
+    
+            if(maior) {
+                printf("Seu número é maior que o número secreto\n");
+            } else {
+                printf("Seu número é menor que o número secreto\n");
+            }
         }
+    
     }
-
     return 0;
 }
-
