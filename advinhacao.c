4d12dfda11cd9d271efa06ed1a604f763137c68d
Bruno <bruno765rez@gmail.com>
2025-10-06 10:40:09 -0300

    Melhorando

--- advinhacao.c
+++ advinhacao.c
@@ -2,7 +2,8 @@
 #include <windows.h>
 #define VIDAS 5
 
-int main() {
+int main()
+{
     SetConsoleOutputCP(CP_UTF8);
 
     int chute;
@@ -12,26 +13,29 @@ int main() {
     printf("= Seja bem vindo ao jogo de advinhação =\n");
     printf("========================================\n");
 
-    for(int i = 1; i <= VIDAS; i++) {
+    for (int i = 1; i <= VIDAS; i++)
+    {
         printf("== Tentativa %d de %d ==\n", i, VIDAS);
         printf("Qual é o seu chute? ");
         scanf("%d", &chute);
         printf("Seu chute foi %d\n", chute);
-    
+
         int acertou = (chute == numerosecreto);
-    
-        if (acertou) {
+        int maior = chute > numerosecreto;
+        int menor = chute < numerosecreto;
+
+        if (acertou)
+        {
             printf("Parabéns! Você acertou!\n");
             break;
-        } else {
-    
-            int maior = chute > numerosecreto;
-    
-            if(maior) {
-                printf("Seu número é maior que o número secreto\n");
-            } else {
-                printf("Seu número é menor que o número secreto\n");
-            }
+        }
+        else if (maior)
+        {
+            printf("Seu número é maior que o número secreto\n");
+        }
+        else if (menor)
+        {
+            printf("Seu número é menor que o número secreto\n");
         }
     }
 
